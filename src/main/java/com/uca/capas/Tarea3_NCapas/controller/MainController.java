2c9b1fa09824091bffa3e0abd286948735d5a786
Fredy Sánchez <00082817@uca.edu.sv>
2020-04-27 23:48:37 -0600

    Dependencia de thymeleaf agregada, aplicacion funcional pero sin terminar

--- src/main/java/com/uca/capas/Tarea3_NCapas/controller/MainController.java
+++ src/main/java/com/uca/capas/Tarea3_NCapas/controller/MainController.java
@@ -1,9 +1,26 @@
 package com.uca.capas.Tarea3_NCapas.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.uca.capas.Tarea3_NCapas.domain.Student;
 
 @Controller
 public class MainController {
 	
-
+	@GetMapping("/ingresar")
+	public String enviarForm(Student student) {
+		return "ingreso";
+	}
+	
+	@PostMapping("/formData")
+	public ModelAndView procesar(Student student) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("correcto");
+		mav.addObject("student", student);
+		
+		return mav;
+	}
 }
