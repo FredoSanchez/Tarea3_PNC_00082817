dbd58e4cb4964e025f29c6ac27f2e56fb50fddc1
Fredy Sánchez <00082817@uca.edu.sv>
2020-04-27 20:40:14 -0600

    Dominio Student creado

---
+++ src/main/java/com/uca/capas/Tarea3_NCapas/domain/Student.java
@@ -0,0 +1,58 @@
+package com.uca.capas.Tarea3_NCapas.domain;
+
+public class Student {
+	private	String nombre;
+	private	String apellidos;
+	private	String fechaNacimiento;
+	private	String lugarNacimiento;
+	private	String procedencia;
+	private	String telefono;
+	private	String movil;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getLugarNacimiento() {
+		return lugarNacimiento;
+	}
+	public void setLugarNacimiento(String lugarNacimiento) {
+		this.lugarNacimiento = lugarNacimiento;
+	}
+	public String getProcedencia() {
+		return procedencia;
+	}
+	public void setProcedencia(String procedencia) {
+		this.procedencia = procedencia;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getMovil() {
+		return movil;
+	}
+	public void setMovil(String movil) {
+		this.movil = movil;
+	}
+	
+	
+	
+}
